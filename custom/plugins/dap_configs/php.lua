5f0427f34e75b6497bed481ba7b88b28619d4d27
Anuragh K P <kpanuragh@gmail.com>
2022-08-24 00:04:15 +0530

    Added my nvchad custom configuration

---
+++ custom/plugins/dap_configs/php.lua
@@ -0,0 +1,8 @@
+local dap = require('dap')
+dap.adapters.php = {
+    type = 'executable',
+    command = 'node',
+    args = { '/home/anuraghkp/.local/share/nvim/mason/packages/php-debug-adapter/extension/out/phpDebug.js' }
+  }
+  
+  
