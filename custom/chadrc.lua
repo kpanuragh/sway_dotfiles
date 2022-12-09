a8f3053b2c2d717b0ce63815b2c312e5ad228cda
Anuragh K P <kpanuragh@gmail.com>
2022-12-10 02:47:25 +0530

    added latest config

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -3,14 +3,13 @@ local pluginConfs = require("custom.plugins.configs")
 local M = {}
 
 M.ui = {
-	theme = "onenord",
+	theme = "everforest",
 }
 M.plugins = {
 	user = require("custom.plugins"),
 	override = {
 		["nvim-treesitter/nvim-treesitter"] = pluginConfs.treesitter,
 		["kyazdani42/nvim-tree.lua"] = pluginConfs.nvimtree,
-		-- ["nvim-telescope/telescope.nvim"] = pluginConfs.telescope,
 	},
 }
 
--- custom/plugins/configs.lua
+++ custom/plugins/configs.lua
@@ -13,6 +13,7 @@ M.treesitter = {
 		"jsdoc",
 		"json",
 		"scss",
+    "go"
 	},
 }
 
